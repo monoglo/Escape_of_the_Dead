4fb4e7cd68341633c402ecb0a2b1a4a4afbe445d
monoglo <monoglo37@gmail.com>
2020-08-11 17:06:25 +0800

    fix(python): fix so many bugs

--- python/game.py
+++ python/game.py
@@ -12,13 +12,17 @@ NO_ZOMBIE_TIMES = 0
 
 def kill_one_zombie():
     global ZOMBIE_NUM
+    global KILLED_ZOMBIE_NUM
     i = random.randint(1, 6)
     if (ZOMBIE_NUM):
         if (i > 2):
+            before_zombie_num = ZOMBIE_NUM
             ZOMBIE_NUM -= 1
-            print('Your dice is %d, you sucessfully killed a zombie!' % i)
+            KILLED_ZOMBIE_NUM += 1
+            print('Your dice is %d, you sucessfully killed a zombie! Zombie num: %d -> %d' %
+                (i, before_zombie_num, ZOMBIE_NUM))
         else:
-            print('Your dice is %d, Failed kill!' % i)
+            print('Your dice is %d, Failed kill! Zombie num: 0 -> 0' % i)
     else:
         print('There is no zombie on the lawn!')
 
@@ -53,22 +57,30 @@ def zombie_coming():
     global ZOMBIE_NUM
     global NO_ZOMBIE_TIMES
     global FIX_PROCESS
-    print('Zombie Coming!!!!')
     if (NO_ZOMBIE_TIMES):
         NO_ZOMBIE_TIMES -= 1
+        print('No Zombie Come!')
+        return
     elif (1 <= FIX_PROCESS & FIX_PROCESS <= 3):
         ZOMBIE_NUM += 1
+        print('1 Zombie Coming!!!!')
     elif (4 <= FIX_PROCESS & FIX_PROCESS <= 5):
         ZOMBIE_NUM += 2
+        print('2 Zombie Coming!!!!')
     elif (6 <= FIX_PROCESS & FIX_PROCESS <= 8):
         ZOMBIE_NUM += 3
-    elif (1 <= FIX_PROCESS & FIX_PROCESS <= 3):
+        print('3 Zombie Coming!!!!')
+    elif (9 <= FIX_PROCESS):
         ZOMBIE_NUM += 4
+        print('4 Zombie Coming!!!!')
 
 
 def zombie_damage():
     global BARRICADE_HP
+    before_hp = BARRICADE_HP
     BARRICADE_HP -= ZOMBIE_NUM
+    print('[Zombie Attack]' + str(ZOMBIE_NUM) + ' zombies damage your Barriage, Barriage health:' +
+        str(before_hp) + '->' + str(BARRICADE_HP))
 
 
 def roll_dice():
@@ -99,12 +111,22 @@ def show_data():
     print('======================')
 
 
+def show_data_result():
+    print('======================')
+    print('ROUND - %d [Result]' % GAME_ROUND)
+    print('Zombies on the lawn: %d' % ZOMBIE_NUM)
+    print('Barriage health: %d' % BARRICADE_HP)
+    print('Car fix process: %d' % FIX_PROCESS)
+    print('======================')
+
+
 def killed_bonus():
     global ZOMBIE_NUM
     global FIX_PROCESS
     global NO_ZOMBIE_TIMES
     global BARRICADE_HP
-    if (killed_bonus >= 10):
+    global KILLED_ZOMBIE_NUM
+    if (KILLED_ZOMBIE_NUM >= 10):
         print('You have killed 10+ zombies, please choose your bonus:')
         print('1.Kill all zombies!')
         print('2.Fix your car for 10%!')
@@ -127,8 +149,9 @@ def main():
         zombie_coming()
         show_data()
         roll_dice()
+        killed_bonus()
         zombie_damage()
-        show_data()
+        show_data_result()
         if (BARRICADE_HP <= 0):
             print('gameover!')
             break
